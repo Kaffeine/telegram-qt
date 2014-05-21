a48cd8e922251ea12635c8807b669ac6c0159686
Alexandr Akulich <akulichalexander@gmail.com>
2014-05-21 15:55:00 +0600

    TLValuesGenerator: Implemented multiply sources and files generation.

--- telegram-qt/generator/main.cpp
+++ telegram-qt/generator/main.cpp
@@ -11,9 +11,10 @@
 
 */
 
-#include <QStringList>
-
+#include <QCoreApplication>
+#include <QDir>
 #include <QFile>
+#include <QStringList>
 #include <QTextStream>
 
 #include <stdio.h>
@@ -34,19 +35,88 @@ inline int indexOfSeparator(const QString &str, int minIndex)
     return dotIndex < underscoreIndex ? dotIndex : underscoreIndex;
 }
 
-int main(int argc, char *argv[])
+bool replaceSection(const QString &fileName, const QString &startMarker, const QString &endMarker, const QString &newContent)
 {
-    if (argc != 2) {
-        printf("Usage: %s <source>\n", argv[0]);
-        return 0;
+    QFile fileToProcess(fileName);
+
+    if (!fileToProcess.open(QIODevice::ReadOnly))
+        return false;
+
+    QString fileContent = fileToProcess.readAll();
+    fileToProcess.close();
+
+    bool winNewLines = fileContent.indexOf(QLatin1String("\r\n")) > 0;
+
+    if (winNewLines) {
+        fileContent.replace(QLatin1String("\r\n"), QLatin1String("\n"));
     }
 
-    QFile sourceFile(QString::fromLatin1(argv[1]));
+    int startPos = fileContent.indexOf(startMarker);
+    int endPos   = fileContent.indexOf(endMarker);
 
-    if (!sourceFile.open(QIODevice::ReadWrite))
-        return -1;
+    if (startPos >= 0) {
+        if (endPos < 0) {
+            printf("There is only start marker in the file %s. Error.\n", fileName.toLocal8Bit().constData());
+            return false;
+        }
+
+        endPos += endMarker.length();
+    } else {
+        return false;
+    }
+
+    const QString previousContent = fileContent.mid(startPos, endPos - startPos);
+
+    if (previousContent == startMarker + newContent + endMarker) {
+        printf("Nothing to do: new and previous contents are exactly same.\n");
+        return true;
+    }
+
+    fileContent.remove(startPos, endPos - startPos);
+    fileContent.insert(startPos, startMarker + newContent + endMarker);
+
+    if (!fileToProcess.open(QIODevice::WriteOnly)) {
+        printf("Can not write file: %s.\n", fileName.toLocal8Bit().constData());
+        return false;
+    }
+
+    if (winNewLines) {
+        fileContent.replace(QLatin1String("\n"), QLatin1String("\r\n"));
+    }
+
+    fileToProcess.write(fileContent.toLatin1());
+
+    fileToProcess.close();
+
+    return true;
+}
+
+bool replacingHelper(const QString &fileName, int spacing, const QString &marker, const QString &newContent)
+{
+    const QString space(spacing, QChar(' '));
+
+    if (!replaceSection(fileName,
+                        QString("%1// Generated %2\n").arg(space).arg(marker),
+                        QString("%1// End of generated %2\n").arg(space).arg(marker), newContent)) {
+        printf("Can not update file %s with marker %2.\n", fileName.toLatin1().constData(), marker.toLatin1().constData());
+        return false;
+    } else {
+        return true;
+    }
+}
+
+QString generateCode(const QString &fileName, int indentation, bool closeEnum)
+{
+    QFile sourceFile(fileName);
+
+    if (!sourceFile.open(QIODevice::ReadWrite)) {
+        return QString();
+    }
 
     QTextStream input(&sourceFile);
+
+    QString result;
+    QString indentationStr(indentation, QChar(' '));
 
     int currentLine = 0;
     while (!input.atEnd()) {
@@ -59,8 +129,8 @@ int main(int argc, char *argv[])
 
         int hashIndex = line.indexOf(QChar('#'));
         if ((hashIndex < 1) || (hashIndex + 1 > line.length())) {
-            printf("Bad string: %s (line %d)\n", line.toLatin1().constData(), currentLine);
-            return -1;
+            printf("Bad string: %s (%s:%d)\n", line.toLocal8Bit().constData(), fileName.toLocal8Bit().constData(), currentLine);
+            return QString();
         }
 
         QString name = line.left(hashIndex);
@@ -70,8 +140,8 @@ int main(int argc, char *argv[])
         int separatorIndex = 0;
         while ((separatorIndex = indexOfSeparator(name, separatorIndex)) > 0) {
             if ((name.length() < separatorIndex + 1) || (!name.at(separatorIndex + 1).isLetter())) {
-                printf("Bad name: %s (line %d)\n", name.toLatin1().constData(), currentLine);
-                return -2;
+                printf("Bad name: %s (%s:%d)\n", name.toLocal8Bit().constData(), fileName.toLocal8Bit().constData(), currentLine);
+                return QString();
             }
             name[separatorIndex + 1] = name.at(separatorIndex + 1).toUpper();
             name.remove(separatorIndex, 1);
@@ -82,12 +152,54 @@ int main(int argc, char *argv[])
         if (endOfValue > 0) {
             value.truncate(endOfValue);
         }
-//        printf("Line %d: name: %s, value: %s\n", currentLine, name.toLatin1().constData(), value.toLatin1().constData());
 
-        if (!input.atEnd())
-            printf("%s = 0x%s,\n", name.toLatin1().constData(), value.toLatin1().constData());
-        else
-            printf("%s = 0x%s\n", name.toLatin1().constData(), value.toLatin1().constData());
+        result.append(indentationStr);
+
+        if (!closeEnum || !input.atEnd()) {
+            result.append(QString("%1 = 0x%2,\n").arg(name).arg(value));
+        } else {
+            result.append(QString("%1 = 0x%2\n").arg(name).arg(value));
+        }
+    }
+
+    return result;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 4) {
+        printf("Usage: %s <source file or directory> <destination file> <marker>\nExample: %s specs ../TLValues.h TLValues\n", argv[0], argv[0]);
+        return 0;
+    }
+
+    QCoreApplication app(argc, argv);
+
+    static const QStringList arguments = app.arguments();
+    static const int indentation = 4;
+    QString indentationStr(indentation, QChar(' '));
+
+    QString code;
+
+    QDir argDir(arguments.at(1));
+
+    if (argDir.exists()) {
+        const QStringList filesList = argDir.entryList(QStringList() << "*.txt", QDir::Files|QDir::Readable, QDir::Name);
+
+        for (int i = 0; i < filesList.count(); ++i) {
+            bool closeEnum = i == filesList.count() - 1;
+            code.append(indentationStr + QString("// From %1:\n").arg(filesList.at(i)));
+            code.append(generateCode(argDir.absolutePath() + "/" + filesList.at(i), indentation, closeEnum));
+
+            if (!closeEnum) {
+                code.append(QChar('\n'));
+            }
+        }
+    } else {
+        code.append(generateCode(arguments.at(1), indentation, /* closeEnum */ true));
+    }
+
+    if (replacingHelper(arguments.at(2), indentation, arguments.at(3), code)) {
+        printf("File successfully updated.\n");
     }
 
     return 0;
