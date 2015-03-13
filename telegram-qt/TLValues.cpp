91df84626432884ccd83b91714f4e674421cf58f
Alexandr Akulich <akulichalexander@gmail.com>
2015-03-13 16:16:42 +0500

    TLValues: Added fallback toString() code.

--- telegram-qt/TLValues.cpp
+++ telegram-qt/TLValues.cpp
@@ -4,6 +4,7 @@
 
 QString TLValue::toString() const
 {
+//    return QString(QLatin1String("%1")).arg(m_value, 8, 16, QLatin1Char('0'));
     static const int index = staticMetaObject.indexOfEnumerator("Value");
     static const QMetaEnum enumerator = staticMetaObject.enumerator(index);
 
