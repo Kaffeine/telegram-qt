438c38aaa25d1f4522e9ebe51c513a3ee90b5641
Alexandr Akulich <akulichalexander@gmail.com>
2015-04-23 23:49:38 +0500

    TLValue::toString(): Added fallback to hex.
    Fallback to hex if the value is not found in the enumerator.

--- telegram-qt/TLValues.cpp
+++ telegram-qt/TLValues.cpp
@@ -17,9 +17,13 @@
 
 QString TLValue::toString() const
 {
-//    return QString(QLatin1String("%1")).arg(m_value, 8, 16, QLatin1Char('0'));
     static const int index = staticMetaObject.indexOfEnumerator("Value");
     static const QMetaEnum enumerator = staticMetaObject.enumerator(index);
 
-    return QString::fromLatin1(enumerator.valueToKey(m_value));
+    const char *value = enumerator.valueToKey(m_value);
+    if (value) {
+        return QString::fromLatin1(value);
+    } else {
+        return QString(QLatin1String("%1")).arg(m_value, 8, 16, QLatin1Char('0'));
+    }
 }
