9512d363f4d60bfb1974d9180b959120bc3a8187
Jesus Fernandez <jsfdez@gmail.com>
2015-05-06 03:01:11 +0200

    added parser

--- cmdApp/main.cpp
+++ cmdApp/main.cpp
@@ -1,8 +1,34 @@
 #include <QCoreApplication>
+#include <CTelegramCore.hpp>
+#include <QCommandLineParser>
+#include <QCommandLineOption>
+
+QCommandLineOption registerOption(QStringList() << "r" << "register", "Register a new <phone_number>.", "phone_number");
 
 int main(int argc, char** argv)
 {
 	QCoreApplication app(argc, argv);
+	QCoreApplication::setApplicationName("Telegram-Qt");
+	QCoreApplication::setApplicationVersion("0.1");
 
-	return app.exec();
+	QCommandLineParser parser;
+	parser.setApplicationDescription("Telegram-Qt for CommandLine");
+	parser.addHelpOption();
+	parser.addVersionOption();
+	parser.addOption(registerOption);
+	parser.addPositionalArgument("secret_file", "Secret file to create/use.");
+	parser.process(app);
+
+	if (!parser.isSet("secret_file"))
+	{
+		parser.showHelp(EXIT_FAILURE);
+	}
+
+	CTelegramCore telegram;
+
+	if (parser.isSet("register"))
+	{
+		QString phoneNumber = parser.value("register");
+		telegram.requestPhoneCode(phoneNumber);
+	}
 }
