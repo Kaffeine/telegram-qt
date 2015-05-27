3409c3f9828310eb9c9fbe2999c8dfeac7b65759
Alexandr Akulich <akulichalexander@gmail.com>
2015-05-27 23:41:57 +0500

    Relicensed under LGPL v2.1 or later.

--- telegram-qt/CTelegramTransport.hpp
+++ telegram-qt/CTelegramTransport.hpp
@@ -1,15 +1,19 @@
 /*
-    Copyright (C) 2014-2015 Alexandr Akulich <akulichalexander@gmail.com>
+   Copyright (C) 2014-2015 Alexandr Akulich <akulichalexander@gmail.com>
 
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-    LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-    OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-    WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+   This file is a part of TelegramQt library.
 
-*/
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+ */
 
 #ifndef CTELEGRAMTRANSPORT_HPP
 #define CTELEGRAMTRANSPORT_HPP
