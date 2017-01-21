4de0177c46469c8750beb36f20e2c50be47dcb3a
Alexandr Akulich <akulichalexander@gmail.com>
2017-01-21 17:31:22 +0500

    testApp/ContactsFilterModel: Added {add,remove,has}Contact methods

--- testApp/CContactsFilterModel.cpp
+++ testApp/CContactsFilterModel.cpp
@@ -2,6 +2,8 @@
 
 #include "CContactModel.hpp"
 
+#include <QDebug>
+
 CContactsFilterModel::CContactsFilterModel(QObject *parent) :
     QSortFilterProxyModel(parent)
 {
@@ -9,7 +11,28 @@ CContactsFilterModel::CContactsFilterModel(QObject *parent) :
 
 void CContactsFilterModel::setFilterList(const QVector<quint32> &filter)
 {
+    qDebug() << Q_FUNC_INFO << filter;
     m_filter = filter;
+    invalidateFilter();
+}
+
+bool CContactsFilterModel::hasContact(quint32 id)
+{
+    return m_filter.contains(id);
+}
+
+void CContactsFilterModel::addContact(quint32 id)
+{
+    if (m_filter.contains(id)) {
+        return;
+    }
+    m_filter.append(id);
+    invalidateFilter();
+}
+
+void CContactsFilterModel::removeContact(quint32 id)
+{
+    m_filter.removeAll(id);
     invalidateFilter();
 }
 
--- testApp/CContactsFilterModel.hpp
+++ testApp/CContactsFilterModel.hpp
@@ -7,7 +7,13 @@ class CContactsFilterModel : public QSortFilterProxyModel
 {
 public:
     explicit CContactsFilterModel(QObject *parent = nullptr);
+
+    QVector<quint32> filter() const { return m_filter; }
     void setFilterList(const QVector<quint32> &filter);
+
+    bool hasContact(quint32 id);
+    void addContact(quint32 id);
+    void removeContact(quint32 id);
 
 protected:
     bool filterAcceptsRow(int sourceRow, const QModelIndex &sourceParent) const override;
