f45727aa8b58a8a265499361de22ff0c019e98c2
Alexander Akulich <akulichalexander@gmail.com>
2017-12-18 20:28:01 +0300

    Generator/tests: Add a test for type members with name conflict

--- generator/tests/tst_Generator/tst_Generator.cpp
+++ generator/tests/tst_Generator/tst_Generator.cpp
@@ -21,6 +21,32 @@ static QByteArray generateTextSpec(const QStringList &types, const QStringList &
     return result;
 }
 
+const QStringList c_sourcesInputMediaDeps =
+{
+    QStringLiteral("inputPhotoEmpty = InputPhoto;"),
+    QStringLiteral("inputFileEmpty = InputFile;"),
+    QStringLiteral("inputVideoEmpty = InputVideo;"),
+    QStringLiteral("inputAudioEmpty = InputAudio;"),
+    QStringLiteral("inputDocumentEmpty = InputDocument;"),
+    QStringLiteral("documentAttributeEmpty = DocumentAttribute;"),
+};
+
+const QStringList c_sourcesInputMedia =
+{
+    QStringLiteral("inputMediaEmpty#9664f57f = InputMedia;"),
+    QStringLiteral("inputMediaUploadedPhoto#f7aff1c0 file:InputFile caption:string = InputMedia;"),
+    QStringLiteral("inputMediaPhoto#e9bfb4f3 id:InputPhoto caption:string = InputMedia;"),
+    QStringLiteral("inputMediaContact#a6e45987 phone_number:string first_name:string last_name:string = InputMedia;"),
+    QStringLiteral("inputMediaUploadedVideo#82713fdf file:InputFile duration:int w:int h:int mime_type:string caption:string = InputMedia;"),
+    QStringLiteral("inputMediaUploadedThumbVideo#7780ddf9 file:InputFile thumb:InputFile duration:int w:int h:int mime_type:string caption:string = InputMedia;"),
+    QStringLiteral("inputMediaVideo#936a4ebd id:InputVideo caption:string = InputMedia;"),
+    QStringLiteral("inputMediaUploadedAudio#4e498cab file:InputFile duration:int mime_type:string = InputMedia;"),
+    QStringLiteral("inputMediaAudio#89938781 id:InputAudio = InputMedia;"),
+    QStringLiteral("inputMediaUploadedDocument#1d89306d file:InputFile mime_type:string attributes:Vector<DocumentAttribute> caption:string = InputMedia;"),
+    QStringLiteral("inputMediaUploadedThumbDocument#ad613491 file:InputFile thumb:InputFile mime_type:string attributes:Vector<DocumentAttribute> caption:string = InputMedia;"),
+    QStringLiteral("inputMediaDocument#1a77f29c id:InputDocument caption:string = InputMedia;"),
+};
+
 class tst_Generator : public QObject
 {
     Q_OBJECT
@@ -32,6 +58,7 @@ public:
 private slots:
     void checkRemoveWord_data();
     void checkRemoveWord();
+    void checkTypeWithMemberConflicts();
 };
 
 tst_Generator::tst_Generator(QObject *parent) :
@@ -92,6 +119,33 @@ void tst_Generator::checkRemoveWord()
     QFETCH(QString, output);
     QFETCH(QString, word);
     QCOMPARE(Generator::removeWord(input, word), output);
+}
+
+void tst_Generator::checkTypeWithMemberConflicts()
+{
+    const QStringList sources = c_sourcesInputMediaDeps + c_sourcesInputMedia;
+    const QString generatedTypeName = Generator::parseLine(c_sourcesInputMedia.first()).typeName;
+    const QByteArray textData = generateTextSpec(sources);
+    Generator generator;
+    QVERIFY(generator.loadFromText(textData));
+    QVERIFY(generator.resolveTypes());
+    QVERIFY(!generator.solvedTypes().isEmpty());
+    const TLType solvedType = getSolvedType(generator, generatedTypeName);
+    QVERIFY(!solvedType.name.isEmpty());
+    const QStringList structMembers = Generator::generateTLTypeMembers(solvedType);
+    static const QStringList checkList = {
+        QStringLiteral("TLInputFile thumb;"),
+        QStringLiteral("TLInputVideo idInputVideo;"),
+        QStringLiteral("TLInputAudio idInputAudio;"),
+        QStringLiteral("TLVector<TLDocumentAttribute> attributes;"),
+    };
+    for (const QString &mustHaveMember : checkList) {
+        if (!structMembers.contains(mustHaveMember)) {
+            // qDebug().noquote() << structMembers.join(QLatin1Char('\n'));
+            QString message = QStringLiteral("The member \"%1\" is missing in the generated struct of the type %2.").arg(mustHaveMember, generatedTypeName);
+            QFAIL(message.toUtf8().constData());
+        }
+    }
 }
 
 QTEST_APPLESS_MAIN(tst_Generator)
