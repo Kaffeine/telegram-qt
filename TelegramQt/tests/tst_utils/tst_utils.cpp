9ad43b068510b860e1e05533494a53b95c75df4b
Alexander Akulich <akulichalexander@gmail.com>
2018-08-27 20:03:20 +0300

    Tests/Utils: Add gzip/ungzip tests

--- TelegramQt/tests/tst_utils/tst_utils.cpp
+++ TelegramQt/tests/tst_utils/tst_utils.cpp
@@ -44,6 +44,10 @@ private slots:
     void testRsaKey();
     void testRsaKeyIsValid();
     void testDeterministicRandom();
+    void testGzipPack();
+    void testGzipUnpack();
+    void testGzipOnDifferentDataSizes_data();
+    void testGzipOnDifferentDataSizes();
 };
 
 tst_utils::tst_utils(QObject *parent) :
@@ -169,6 +173,127 @@ void tst_utils::testDeterministicRandom()
                  "953abec4424f3cc56f35d17e47d8bc3a7a525f73b3d900dfca930e72955695b6"
                  "60b692035c79002440e77dfa5e893a2b51e2820e7c43ac11da6331a96b636bb3")
              );
+}
+
+const QByteArray c_gzipPackedData = QByteArray::fromHex(
+            QByteArrayLiteral(
+                "1f8b08000000000000034be16c99c300"
+                "04cbf736472b9f6a8e369f5eb98709c8"
+                "173db2554610489f3976831524cf08c4"
+                "7c8626967a86a6267a86e6a67aa6060c"
+                "bb1921f20204e419a1f2ea4606068656"
+                "06494616566946c6295669602e10a012"
+                "89307d207b9950cc3533d7333544b117"
+                "9f3c23541e6aaf9979b2958149aa05c8"
+                "5e2342f63203313fb27f0c0d0c90edc5"
+                "27cf0895c7e25f6342f6b2a0fbc712d5"
+                "bff8e419a1f258fc6b82cf5e26a83e84"
+                "7fccccf40c8de0fe6126cddc2464f7b0"
+                "6284431a489f293ef70840f5f15a1a02"
+                "83d542cf14ec1a0606e4704297373587"
+                "c89f008a2f6863644801d207ae303274"
+                "083330189432303c98cc02a6ef003572"
+                "01e5125e41d206483dc36220eb950a83"
+                "1cd4dc062b4e30ade0c7c030219e11ac"
+                "4f401d685f46494941b195be7e895e6e"
+                "aa3e248f80b403008b1f9e4c44030000"
+                ));
+
+const QByteArray c_gzipUnpackedData = QByteArray::fromHex(
+            QByteArrayLiteral(
+                "6409849c00000000a7bd835b23ca835b"
+                "379779bc0200000015c4b51c11000000"
+                "ccc6d80500000000010000000e313439"
+                "2e3135342e3137352e353000bb010000"
+                "ccc6d80510000000010000000e313439"
+                "2e3135342e3137352e353000bb010000"
+                "ccc6d805010000000100000027323030"
+                "313a306232383a663233643a66303031"
+                "3a303030303a303030303a303030303a"
+                "30303061bb010000ccc6d80500000000"
+                "020000000e3134392e3135342e313637"
+                "2e353100bb010000ccc6d80510000000"
+                "020000000e3134392e3135342e313637"
+                "2e353100bb010000ccc6d80501000000"
+                "0200000027323030313a303637633a30"
+                "3465383a663030323a303030303a3030"
+                "30303a303030303a30303061bb010000"
+                "ccc6d80500000000030000000f313439"
+                "2e3135342e3137352e313030bb010000"
+                "ccc6d80510000000030000000f313439"
+                "2e3135342e3137352e313030bb010000"
+                "ccc6d805010000000300000027323030"
+                "313a306232383a663233643a66303033"
+                "3a303030303a303030303a303030303a"
+                "30303061bb010000ccc6d80500000000"
+                "040000000e3134392e3135342e313637"
+                "2e393100bb010000ccc6d80510000000"
+                "040000000e3134392e3135342e313637"
+                "2e393100bb010000ccc6d80501000000"
+                "0400000027323030313a303637633a30"
+                "3465383a663030343a303030303a3030"
+                "30303a303030303a30303061bb010000"
+                "ccc6d80502000000040000000f313439"
+                "2e3135342e3136362e313230bb010000"
+                "ccc6d805030000000400000027323030"
+                "313a303637633a303465383a66303034"
+                "3a303030303a303030303a303030303a"
+                "30303062bb010000ccc6d80501000000"
+                "0500000027323030313a306232383a66"
+                "3233663a663030353a303030303a3030"
+                "30303a303030303a30303061bb010000"
+                "ccc6d80510000000050000000d39312e"
+                "3130382e35362e3132300000bb010000"
+                "ccc6d80500000000050000000d39312e"
+                "3130382e35362e3135370000bb010000"
+                "c8000000a086010064000000c0d40100"
+                "8813000030750000e093040030750000"
+                "dc0500000a00000060ea000002000000"
+                "c800000000a3020000ea24001e000000"
+                "05000000803a090005000000204e0000"
+                "905f010030750000102700000d687474"
+                "70733a2f2f742e6d652f000015c4b51c"
+                "00000000"
+                ));
+
+void tst_utils::testGzipPack()
+{
+    QByteArray result = Utils::packGZip(c_gzipUnpackedData);
+    QCOMPARE(result.toHex(), c_gzipPackedData.toHex());
+}
+
+void tst_utils::testGzipUnpack()
+{
+    QByteArray result = Utils::unpackGZip(c_gzipPackedData);
+    QCOMPARE(result.toHex(), c_gzipUnpackedData.toHex());
+}
+
+void tst_utils::testGzipOnDifferentDataSizes_data()
+{
+    QTest::addColumn<uint>("dataSize");
+    QTest::newRow("Empty") << 0u;
+    QTest::newRow("Small") << Utils::c_gzipBufferSize / 2;
+    QTest::newRow("Exact buffer size") << Utils::c_gzipBufferSize;
+    QTest::newRow("Twice as big") << Utils::c_gzipBufferSize * 2;
+    QTest::newRow("Really big") << Utils::c_gzipBufferSize * 10;
+    QTest::newRow("Big (not aligned 1)") << Utils::c_gzipBufferSize * 2 + 1;
+    QTest::newRow("Big (not aligned 2)") << Utils::c_gzipBufferSize * 2 + 2;
+    QTest::newRow("Big (not aligned 3)") << Utils::c_gzipBufferSize * 2 + 3;
+}
+
+void tst_utils::testGzipOnDifferentDataSizes()
+{
+    QFETCH(uint, dataSize);
+    const int dataSizeInt = static_cast<int>(dataSize);
+
+    DeterministicGenerator deterministic;
+    RandomGeneratorSetter generatorKeeper(&deterministic);
+    QByteArray data = Utils::getRandomBytes(dataSizeInt);
+
+    QByteArray packed = Utils::packGZip(data);
+    QVERIFY(!packed.isEmpty());
+    QByteArray unpacked = Utils::unpackGZip(packed);
+    QCOMPARE(unpacked.size(), dataSizeInt);
 }
 
 QTEST_APPLESS_MAIN(tst_utils)
