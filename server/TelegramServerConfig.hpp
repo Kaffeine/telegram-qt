9c4277be079e55ff62b0467c2bfa6da9c4d2f5ff
Alexey Min <alexey.min@gmail.com>
2019-02-05 09:40:04 +0300

    Server: add config class

---
+++ server/TelegramServerConfig.hpp
@@ -0,0 +1,48 @@
+/*
+   Copyright (C) 2019 Alexey Minnekhanov <alexey.min@gmail.com>
+
+   This file is a part of TelegramQt library.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+ */
+
+#ifndef TELEGRAM_SERVER_CONFIG_HPP
+#define TELEGRAM_SERVER_CONFIG_HPP
+
+#include "DcConfiguration.hpp"
+
+namespace Telegram {
+
+namespace Server {
+
+class Config {
+public:
+    explicit Config(const QString &fileName = QString());
+
+    QString fileName() const { return m_fileName; }
+    DcConfiguration serverConfiguration() const { return m_serverConfiguration; }
+    QString privateKeyFile() const { return m_privateKeyFile; }
+
+    bool load();
+    bool save() const;
+
+private:
+    QString m_fileName;
+    QString m_privateKeyFile;
+    DcConfiguration m_serverConfiguration;
+};
+
+} // Server namespace
+
+} // Telegram namespace
+
+#endif // TELEGRAM_SERVER_CONFIG_HPP
