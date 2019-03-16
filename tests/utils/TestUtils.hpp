e3b0191f5795d113959bce3a4f6737aa3efdb35d
Alexander Akulich <akulichalexander@gmail.com>
2019-03-16 18:32:17 +0300

    Tests/Utils: Add COMPARE_PEERS

--- tests/utils/TestUtils.hpp
+++ tests/utils/TestUtils.hpp
@@ -10,5 +10,10 @@
 #define TRY_COMPARE(expr, expected) QTRY_COMPARE_WITH_TIMEOUT(expr, expected, TEST_TIMEOUT)
 #define TRY_VERIFY(expr) QTRY_VERIFY_WITH_TIMEOUT(expr, TEST_TIMEOUT)
 #define TRY_VERIFY2(expr, messageExpression) QTRY_VERIFY2_WITH_TIMEOUT(expr, messageExpression, TEST_TIMEOUT)
+#define COMPARE_PEERS(actual, expected) \
+    do {\
+        if (!QTest::qCompare(actual.toString(), expected.toString(), #actual, #expected, __FILE__, __LINE__))\
+            return;\
+    } while (false)
 
 #endif // TELEGRAMQT_TEST_UTILS_HPP
