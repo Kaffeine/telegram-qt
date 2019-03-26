f59764584a285e157bf1a9976502553c86304140
Alexander Akulich <akulichalexander@gmail.com>
2019-03-26 23:49:26 +0300

    Add TelegramTestServer (that runs with well-known private key)

---
+++ tests/manual/server/main.cpp
@@ -0,0 +1,115 @@
+/*
+   Copyright (C) 2017 Alexandr Akulich <akulichalexander@gmail.com>
+
+   This file is a part of TelegramQt library.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+ */
+
+#include "TelegramServerConfig.hpp"
+#include "TelegramServerUser.hpp"
+#include "DcConfiguration.hpp"
+#include "DefaultAuthorizationProvider.hpp"
+#include "LocalCluster.hpp"
+#include "Session.hpp"
+
+// Test keys
+#include "keys_data.hpp"
+
+#include "Utils.hpp"
+#include <QCoreApplication>
+#include <QDebug>
+#include <QStandardPaths>
+#include <QCommandLineParser>
+
+using namespace Telegram::Server;
+
+class DBusCodeAuthProvider : public Authorization::DefaultProvider
+{
+public:
+    static QString s_code;
+protected:
+    Authorization::Code generateCode(Session *session, const QString &identifier) override;
+};
+
+QString DBusCodeAuthProvider::s_code = QStringLiteral("11111");
+
+Authorization::Code DBusCodeAuthProvider::generateCode(Session *session, const QString &identifier)
+{
+    Authorization::Code code = DefaultProvider::generateCode(session, identifier);
+    code.code = s_code;
+    return code;
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    a.setOrganizationName(QLatin1String("TelegramQt"));
+    a.setApplicationName(QLatin1String("TelegramTestServer"));
+
+    Telegram::initialize();
+    if (!TestKeyData::initKeyFiles()) {
+        qCritical() << "Unable to init RSA key files.";
+        return -1;
+    }
+
+    const Telegram::RsaKey privateKey = Telegram::RsaKey::fromFile(TestKeyData::privateKeyFileName());
+    if (!privateKey.isValid()) {
+        qCritical() << "Unable to read RSA key.";
+        return -1;
+    }
+
+    QCommandLineParser parser;
+    parser.addHelpOption();
+
+    QCommandLineOption ipAddressOption(QStringList({ QLatin1String("a"), QLatin1String("address") }));
+    ipAddressOption.setValueName(QLatin1String("ip"));
+    ipAddressOption.setDefaultValue(QLatin1String("127.0.0.1"));
+    parser.addOption(ipAddressOption);
+
+    QCommandLineOption portOption(QStringList({ QLatin1String("p"), QLatin1String("port") }));
+    portOption.setDescription(QLatin1String("first port"));
+    portOption.setValueName(QLatin1String("port"));
+    portOption.setDefaultValue(QLatin1String("10443"));
+    parser.addOption(portOption);
+
+    parser.process(a);
+
+    Telegram::DcConfiguration dcConfig;
+    for (quint32 i = 0; i < 3; ++i) {
+        Telegram::DcOption dcOption;
+        dcOption.id = i + 1;
+        dcOption.flags = Telegram::DcOption::TcpOnly;
+        dcOption.address = parser.value(ipAddressOption);
+        dcOption.port = static_cast<quint16>(parser.value(portOption).toUInt() + i);
+        dcConfig.dcOptions.append(dcOption);
+    }
+
+    LocalCluster cluster;
+    cluster.setServerPrivateRsaKey(privateKey);
+    cluster.setServerConfiguration(dcConfig);
+
+    DBusCodeAuthProvider authProvider;
+    cluster.setAuthorizationProvider(&authProvider);
+    qInfo() << "Custom auth code provider enabled. The code:" << DBusCodeAuthProvider::s_code;
+    qInfo() << "Start server with unsafe (not secret) encryption RSA key:";
+    qInfo() << "    Private:" << TestKeyData::privateKeyFileName();
+    qInfo() << "    Public PKCS1:" << TestKeyData::publicKeyPkcs1FileName();
+    qInfo() << "    Public PKCS8:" << TestKeyData::publicKeyPkcs8FileName();
+    if (!cluster.start()) {
+        return -2;
+    }
+
+    int retCode = a.exec();
+    TestKeyData::cleanupKeyFiles();
+    return retCode;
+}
