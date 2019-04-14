8bc12173fb5837e1e6e9d09117cc1a0a1ed7a6d2
Alexander Akulich <akulichalexander@gmail.com>
2019-04-14 10:25:11 +0300

    Tests/ServerUtils: Reformat the code

--- tests/utils/TestServerUtils.hpp
+++ tests/utils/TestServerUtils.hpp
@@ -8,16 +8,16 @@
 Telegram::Server::LocalUser *tryAddUser(Telegram::Server::LocalCluster *cluster, const UserData &data)
 {
     Telegram::Server::LocalUser *u = cluster->addUser(data.phoneNumber, data.dcId);
-    if (u) {
-        u->setFirstName(data.firstName);
-        u->setLastName(data.lastName);
-        if (!data.passwordHash.isEmpty()) {
-            u->setPassword(data.passwordSalt, data.passwordHash);
-        } else {
-            u->setPlainPassword(data.password);
-        }
-    } else {
+    if (!u) {
         qCritical() << "Unable to add a user";
+        return nullptr;
+    }
+    u->setFirstName(data.firstName);
+    u->setLastName(data.lastName);
+    if (!data.passwordHash.isEmpty()) {
+        u->setPassword(data.passwordSalt, data.passwordHash);
+    } else {
+        u->setPlainPassword(data.password);
     }
     return u;
 }
