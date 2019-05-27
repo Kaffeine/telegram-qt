089fa9202a7d0c30e43a1780cf92c18d6c72da6c
Alexander Akulich <akulichalexander@gmail.com>
2019-05-27 20:14:23 +0300

    QmlClient: Introduce ItemModelExtensions class

---
+++ clients/qml-client/models/ItemModelExtensions.hpp
@@ -0,0 +1,13 @@
+#ifndef ITEMMODELEXTENSIONS_HPP
+#define ITEMMODELEXTENSIONS_HPP
+
+class ItemModelExtensions
+{
+public:
+    enum Mode {
+        CallModelApi,
+        BypassModelApi,
+    };
+};
+
+#endif // ITEMMODELEXTENSIONS_HPP
