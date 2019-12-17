cd5bc1f7fd1f76c9f97792f662389bf49f348e9f
Alexander Akulich <akulichalexander@gmail.com>
2019-12-18 00:07:55 +0300

    ClientRpcLayer: Use invokeMethod(Functor) if available

--- TelegramQt/ClientRpcLayer.cpp
+++ TelegramQt/ClientRpcLayer.cpp
@@ -393,7 +393,11 @@ QByteArray RpcLayer::getInitConnection() const
 void RpcLayer::addMessageToAck(quint64 messageId)
 {
     if (m_messagesToAck.isEmpty()) {
+#if QT_VERSION >= QT_VERSION_CHECK(5, 10, 0)
+        QMetaObject::invokeMethod(this, &RpcLayer::acknowledgeMessages, Qt::QueuedConnection);
+#else
         QMetaObject::invokeMethod(this, "acknowledgeMessages", Qt::QueuedConnection);
+#endif
     }
     m_messagesToAck.append(messageId);
 }
